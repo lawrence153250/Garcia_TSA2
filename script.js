9b45eae4e4cb2398d2683b39fe05c8ca540be3bc
lawrence153250 <lawrencegarcia153250@gmail.com>
2024-02-07 23:37:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function moveBox(color) {
     var box1 = document.getElementById('box1');
     var box2 = document.getElementById('box2');
 
-    box1.style.transition = 'top 1.5s ease-in-out, background-color 2.5s ease-out';
-    box2.style.transition = 'top 1.5s ease-in-out, background-color 2.5s ease-out';
+    box1.style.transition = 'top 2s ease-in-out, background-color 2.5s ease-out';
+    box2.style.transition = 'top 2s ease-in-out, background-color 2.5s ease-out';
 
     if (box1.style.top === '300px') {
         box1.style.top = '-300px';
